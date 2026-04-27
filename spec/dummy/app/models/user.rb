2f7185c58dc4b38701964151b8cfabff1bfdbc55
Aleksander Jodłowski <and0941@gmail.com>
2026-04-27 13:56:01 +0200

    added user model to dummy app

---
+++ spec/dummy/app/models/user.rb
@@ -0,0 +1,3 @@
+class User < ApplicationRecord
+  include Pheidi::Participant
+end
---
+++ spec/dummy/config/initializers/pheidi.rb
@@ -0,0 +1,4 @@
+Pheidi.configure do |config|
+  # The class name of your user model (e.g., "User", "Account", "Admin")
+  config.user_class = "User"
+end
